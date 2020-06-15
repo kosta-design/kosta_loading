4dc8bb92d384fa082e9827be8b5231d65ae1cfcd
kosta-design <66972581+kosta-design@users.noreply.github.com>
2020-06-15 22:13:04 +0200

    Add files via upload
    **PRE-WARNING: Note that i’m new to this. If you find any problems,
    contact me through discord for a faster solve.**
    
    Updates are coming. I might re-make the whole loadingscreen
    for a more simple understanding.

---
+++ kosta-loading/__resource.lua
@@ -0,0 +1,34 @@
+files {
+    'loading.html',
+    'css/bootstrap.min.css',
+    'css/css.css',
+    'css/demo.css',
+    'css/font.css',
+    'css/fontawesome-all.min.css',
+    'css/reset.css',
+    'css/style.css',
+    'css/style_common.css',
+    'css/style-dark.css',
+    'css/style1.css',
+    'img/back001.jpg',
+    'img/discord-icon.png',
+    'img/discord-icon2.png',
+    'img/discord-logo.png',
+    'img/gta.jpg',
+    'img/png.png',
+    'img/white_texture.jpg',
+    'js/main.js',
+    'js/music-controls.js',
+    'js/music-handler.js',
+    'js/video.js',
+    'js/script.js',
+    'webfonts/BAUHS93.woff',
+    'webfonts/HARLOWSI_1.ttf',
+    'webfonts/YanoneKaffeesatz-VariableFont_wght.ttf',
+    'js/video.js',
+    'webfonts/Inconsolata-VariableFont_wdth,wght.ttf',
+}
+
+loadscreen 'loading.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
